7a07b664463885448321da818cc428a4d1591f7c
FAERIS <firstlnw0099@gmail.com>
2024-01-22 02:34:22 +0700

    edit badge

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardHeader } from "./ui/card";
 import { Github, Link2Icon, LinkIcon } from "lucide-react";
-import { Badge } from "./ui/Badge";
+import { Badge } from "@/components/Badge";
 
 const ProjectCard = ({ project }) => {
   return (
